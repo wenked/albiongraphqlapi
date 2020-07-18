935cacce5de7ace998ca73f4017dc17886d5e092
wenked <wenked.matheus@yahoo.com.br>
2020-07-18 05:52:51 -0300

    ok

--- dist/albionDataSource.js
+++ dist/albionDataSource.js
@@ -7,10 +7,7 @@ exports.AlbionApiDataSource = void 0;
 const apollo_datasource_rest_1 = require("apollo-datasource-rest");
 const zergUtils_1 = require("./utils/zergUtils");
 const lodash_1 = __importDefault(require("lodash"));
-const sortTopFame = (a, b) => b.killFame - a.killFame;
-const reducer = (acc, vAt) => {
-    return acc + vAt.killFame;
-};
+const resultHandler_1 = require("./utils/resultHandler");
 class AlbionApiDataSource extends apollo_datasource_rest_1.RESTDataSource {
     constructor() {
         super();
@@ -34,11 +31,11 @@ class AlbionApiDataSource extends apollo_datasource_rest_1.RESTDataSource {
         });
     }
     async getBattleById(id) {
-        console.log(id);
         let offset = 0;
         let events = [];
         const killboard = await this.get(`battles/${id}`);
         let data = await this.get(`events/battle/${id}?offset=${offset}&limit=51`);
+        const playersKb = lodash_1.default.map(killboard.players, (players) => players);
         for (offset = 0; data.length > 0; offset += 50) {
             data = await this.get(`events/battle/${id}?offset=${offset}&limit=51`);
             events.push(data);
@@ -69,7 +66,7 @@ class AlbionApiDataSource extends apollo_datasource_rest_1.RESTDataSource {
         }));
         const noFilterPlayersInfo = playersWithItems.flat();
         let playersInfo = lodash_1.default.uniqBy(noFilterPlayersInfo, 'id');
-        const newPlayersObj = playersInfo
+        const playersArray = playersInfo
             .map((player) => {
             if (player !== undefined &&
                 killboard.players[player === null || player === void 0 ? void 0 : player.id] !== undefined) {
@@ -88,14 +85,12 @@ class AlbionApiDataSource extends apollo_datasource_rest_1.RESTDataSource {
                 return Object.assign(Object.assign({}, player), { averageIp: newplayer === null || newplayer === void 0 ? void 0 : newplayer.AverageItemPower });
             }
         });
-        console.log(newPlayersObj);
-        const playersKb = lodash_1.default.map(killboard.players, (players) => players);
         const mergePlayers = lodash_1.default.map(playersKb, (player) => {
-            let player2 = lodash_1.default.find(newPlayersObj, { id: player.id });
+            let player2 = lodash_1.default.find(playersArray, { id: player.id });
             if (player2 !== undefined) {
                 return player2;
             }
-            return Object.assign(Object.assign({}, player), { role: '', weapon: '', averageIp: null });
+            return Object.assign(Object.assign({}, player), { role: '', weapon: '', averageIp: 0 });
         });
         const mergeWithItems = mergePlayers.map((player2) => {
             if (player2.role !== '') {
@@ -103,112 +98,61 @@ class AlbionApiDataSource extends apollo_datasource_rest_1.RESTDataSource {
             }
             if (player2.killFame > 0) {
                 let newplayer = killersAndAssistsEvents[player2.id];
-                return Object.assign(Object.assign({}, player2), { weapon: newplayer !== undefined &&
-                        newplayer !== undefined &&
-                        null &&
-                        newplayer.Equipment.MainHand.Type, role: newplayer !== undefined &&
-                        newplayer !== undefined &&
-                        null &&
-                        zergUtils_1.getRole(newplayer.Equipment.MainHand.Type), averageIp: newplayer !== undefined && newplayer.AverageItemPower
+                return Object.assign(Object.assign({}, player2), { weapon: newplayer !== undefined
+                        ? newplayer.Equipment.MainHand !== null
+                            ? newplayer.Equipment.MainHand.Type
+                            : ''
+                        : '', role: newplayer !== undefined
+                        ? newplayer.Equipment.MainHand !== null
+                            ? zergUtils_1.getRole(newplayer.Equipment.MainHand.Type)
+                            : ''
+                        : '', averageIp: newplayer !== undefined && newplayer.AverageItemPower
                         ? newplayer.AverageItemPower
-                        : null });
+                        : 0 });
             }
             let newplayer = deathEvents[player2.id];
-            return Object.assign(Object.assign({}, player2), { weapon: newplayer !== undefined &&
-                    newplayer !== undefined &&
-                    null &&
-                    newplayer.Equipment.MainHand.Type, role: newplayer !== undefined &&
-                    newplayer !== undefined &&
-                    null &&
-                    zergUtils_1.getRole(newplayer.Equipment.MainHand.Type), averageIp: newplayer !== undefined && newplayer.AverageItemPower
+            return Object.assign(Object.assign({}, player2), { weapon: newplayer !== undefined
+                    ? newplayer.Equipment.MainHand !== null
+                        ? newplayer.Equipment.MainHand.Type
+                        : ''
+                    : '', role: newplayer !== undefined
+                    ? newplayer.Equipment.MainHand !== null
+                        ? zergUtils_1.getRole(newplayer.Equipment.MainHand.Type)
+                        : ''
+                    : '', averageIp: newplayer !== undefined && newplayer.AverageItemPower
                     ? newplayer.AverageItemPower
-                    : null });
+                    : 0 });
         });
-        const handlerResult = () => {
-            const arrAlly = lodash_1.default.map(killboard.alliances, (alliance) => alliance).sort(sortTopFame);
-            const arrGuild = lodash_1.default.map(killboard.guilds, (guild) => guild)
-                .sort(sortTopFame)
-                .map((guild) => {
-                return Object.assign(Object.assign({}, guild), { totalPlayers: lodash_1.default.map(killboard.players, (players) => players).filter((player) => player.guildName === guild.name).length });
-            });
-            const winnerAlly = arrAlly[0];
-            const winnerGuild = arrGuild[0];
-            if (winnerGuild.killFame > winnerAlly.killFame) {
-                const winnerGuilds = arrGuild.filter((guild) => guild.name === winnerGuild.name);
-                const losersGuilds = arrGuild.filter((guild) => guild.name !== winnerGuild.name);
-                const playersWinners = mergeWithItems.filter((player) => player.guildName === winnerGuild.name);
-                const playersLosers = mergeWithItems.filter((player) => player.guildName !== winnerGuild.name);
-                return {
-                    loserAllys: arrAlly,
-                    winnerAllys: [],
-                    winnerGuilds: winnerGuilds,
-                    loserGuilds: losersGuilds,
-                    winnerTotalFame: winnerGuild.killFame,
-                    winnerTotalKIlls: winnerGuild.kills,
-                    winnerTotalDeaths: winnerGuild.deaths,
-                    loserTotalFame: arrAlly.reduce(reducer, 0),
-                    loserTotalDeaths: arrAlly.reduce((acc, vat) => {
-                        return acc + vat.deaths;
-                    }, 0),
-                    loserTotalKills: arrAlly.reduce((acc, vat) => {
-                        return acc + vat.kills;
-                    }, 0),
-                    playersWinners,
-                    playersLosers,
-                };
-            }
-            const winnerAllys = arrAlly.filter((guild) => guild.name === winnerAlly.name);
-            const loserAllys = arrAlly.filter((guild) => guild.name !== winnerAlly.name);
-            const winnerTotalFame = winnerAllys.reduce(reducer, 0);
-            const loserTotalFame = loserAllys.reduce(reducer, 0);
-            const winnerGuilds = arrGuild.filter((guild) => guild.alliance === winnerAlly.name);
-            const loserGuilds = arrGuild.filter((guild) => guild.alliance !== winnerAlly.name);
-            const playersWinners = mergeWithItems.filter((player) => player.allianceName === winnerAlly.name);
-            const playersLosers = mergeWithItems.filter((player) => player.allianceName !== winnerAlly.name);
-            return {
-                loserAllys,
-                winnerAllys,
-                winnerGuilds,
-                loserGuilds,
-                winnerTotalFame,
-                loserTotalFame,
-                playersWinners,
-                playersLosers,
-                winnerTotalKIlls: winnerAlly.kills,
-                winnerTotalDeaths: winnerAlly.deaths,
-                loserTotalKills: loserAllys.reduce((acc, vat) => {
-                    return acc + vat.kills;
-                }, 0),
-                loserTotalDeaths: loserAllys.reduce((acc, vat) => {
-                    return acc + vat.deaths;
-                }, 0),
-            };
-        };
+        const teste = mergeWithItems.map((player) => {
+            return Object.assign(Object.assign({}, player), { averageIp: player.averageIp == null ? 0 : player.averageIp });
+        });
+        console.log(teste);
+        const { winnerGuilds, winnerAllys, loserAllys, loserGuilds, loserTotalDeaths, loserTotalFame, loserTotalKills, playersLosers, playersWinners, winnerTotalDeaths, winnerTotalFame, winnerTotalKIlls, } = resultHandler_1.resultHandler(killboard, mergeWithItems);
         return {
             battleId: killboard.id,
             totalKills: killboard.totalKills,
             totalFame: killboard.totalFame,
-            players: newPlayersObj,
+            players: playersArray,
             startTime: killboard.startTime,
             endTime: killboard.endTime,
             totalPlayers: lodash_1.default.map(killboard.players, (players) => players).length,
             winners: {
-                alliances: handlerResult().winnerAllys,
-                guilds: handlerResult().winnerGuilds,
-                totalFame: handlerResult().winnerTotalFame,
-                players: handlerResult().playersWinners.sort((a, b) => b.killFame - a.killFame),
-                kills: handlerResult().winnerTotalKIlls,
-                deaths: handlerResult().winnerTotalDeaths,
-                totalPlayers: handlerResult().playersWinners.length,
+                alliances: winnerAllys,
+                guilds: winnerGuilds,
+                totalFame: winnerTotalFame,
+                players: playersWinners.sort((a, b) => b.killFame - a.killFame),
+                kills: winnerTotalKIlls,
+                deaths: winnerTotalDeaths,
+                totalPlayers: playersWinners.length,
             },
             losers: {
-                alliances: handlerResult().loserAllys,
-                guilds: handlerResult().loserGuilds,
-                totalFame: handlerResult().loserTotalFame,
-                players: handlerResult().playersLosers.sort((a, b) => b.killFame - a.killFame),
-                kills: handlerResult().loserTotalKills,
-                deaths: handlerResult().loserTotalDeaths,
-                totalPlayers: handlerResult().playersLosers.length,
+                alliances: loserAllys,
+                guilds: loserGuilds,
+                totalFame: loserTotalFame,
+                players: playersLosers.sort((a, b) => b.killFame - a.killFame),
+                kills: loserTotalKills,
+                deaths: loserTotalDeaths,
+                totalPlayers: playersLosers.length,
             },
         };
     }
